0f0d95a24f2e6e60401ffdf9aca22670ae80e354
naiaragabriela <naigabi31@gmail.com>
2024-10-02 08:30:29 -0300

    Feat: add CSS

---
+++ pages/css/integracao.jsx
@@ -0,0 +1,12 @@
+//não posso impotar o css aqui
+// o import foi feito dentro de _app.js
+
+export default function integracao() {
+    return (
+        <div className="integracao1">
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branca">Texto #03</div>
+        </div>
+    )
+}
